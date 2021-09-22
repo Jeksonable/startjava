3c9ef26d9a82f7238da78ce7a930ddb565c30579
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-09-23 00:14:57 +0700

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    
+    public static void main(String[] args) {
+        byte coreNums = 4;
+        short ramMemory = 8;
+        int bitDepth = 64;
+        long batteryCapacity = 48840;
+        float freqCpu = 2.6f;
+        double diskSize = 808.5;
+        char windowsVersion = '7';
+        boolean personal = true;
+
+        System.out.println(coreNums);
+        System.out.println(ramMemory);
+        System.out.println(bitDepth);
+        System.out.println(batteryCapacity);
+        System.out.println(freqCpu);
+        System.out.println(diskSize);
+        System.out.println(windowsVersion);
+        System.out.println(personal);
+    }
+}
