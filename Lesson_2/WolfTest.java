3230d7903c19ae3d2ac4295f49daa5c32e27c327
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-09-27 23:12:28 +0700

    implement HW_3 Lesson_2

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,14 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.sex = "самец";
-        wolf.nickname = "Альфа";
-        wolf.weight = 48.7;
-        wolf.age = 6;
-        wolf.color = "черный";
+        wolf.setSex("самец");
+        wolf.setNickname("Альфа");
+        wolf.setWeight(48.7);
+        wolf.setAge(6);
+        wolf.setColor("черный");
 
-        System.out.println("Перед нами " + wolf.sex + " волка по кличке " + wolf.nickname + ".");
-        System.out.println("Возраст составляет " + wolf.age + " лет, вес - " + wolf.weight + "кг, цвет - " + wolf.color + ".");
+        System.out.println("Перед нами " + wolf.getSex() + " волка по кличке " + wolf.getNickname() + ".");
+        System.out.println("Возраст составляет " + wolf.getAge() + " лет, вес - " + wolf.getWeight() + "кг, цвет - " + wolf.getColor() + ".");
         System.out.println("Животное сначала " + wolf.sit() + ", потом " + wolf.walk() + ", а затем " + wolf.run() + ".");
         System.out.println("После этого волк " + wolf.howl() + " и " + wolf.hunt() + ".");
     }
