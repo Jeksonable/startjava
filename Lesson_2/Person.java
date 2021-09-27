31436bde26839b24a1d866e24064cc683352d421
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-09-27 21:19:16 +0700

    implement HW_1 Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "Мужской";
+    String name = "Евгений";
+    double height = 1.85;
+    double weight = 98.7;
+    int age = 25;
+
+    public void walk() {
+        System.out.println("Человек пошел.");
+    }
+
+    public void sit() {
+        System.out.println("Человек сел.");
+    }
+
+    public void run() {
+        System.out.println("Человек побежал.");
+    }
+
+    public void talk() {
+        System.out.println("Человек говорит.");
+    }
+
+    public void learnJava() {
+        System.out.println("Человек учит Java.");
+    }
+}
