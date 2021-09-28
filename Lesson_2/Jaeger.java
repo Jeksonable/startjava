9194edaf7b0c7c23d8df44e51ed87985afc29482
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-09-29 00:31:59 +0700

    implement HW_5 Lesson_2

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,115 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int strength;
+    private int armor;
+    private boolean isDestroyed;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public boolean getIsDestroyed() {
+        return isDestroyed;
+    }
+
+    public void setIsDestroyed(boolean isDestroyed) {
+        this.isDestroyed = isDestroyed;
+    }
+
+    public void drift() {
+        System.out.println("Экипаж робота " + modelName + " входит в дрифт.");
+    }
+
+    public String move() {
+        return "Начинаем движение!";
+    }
+
+    public String scanKaiju() {
+        return "Сканирую!";
+    }
+
+    public void useCannon() {
+        System.out.println("Орудия к бою!");
+    }
+
+    public void compareStrength(int strength) {
+        if (this.strength < strength) {
+            System.out.println("Егерь " + modelName + " слабее!");
+        } else if (this.strength > strength) {
+            System.out.println("Егерь " + modelName + " сильнее!");
+        } else {
+            System.out.println("Роботы равны по силе.");
+        }
+    }
+
+    public void compareArmor(int armor) {
+        if (this.armor < armor) {
+            System.out.println("Егерь " + modelName + " слабее бронирован!");
+        } else if (this.armor > armor) {
+            System.out.println("Егерь " + modelName + " сильнее бронирован!");
+        } else {
+            System.out.println("Роботы одинаково защищены.");
+        }
+    }
+
+    public void destroy() {
+        System.out.println("Кайдзю уничтожил егеря " + modelName + "!");
+        isDestroyed = true;
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,40 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Gipsy Danger");
+        jaeger1.setMark("Mark-3");
+        jaeger1.setOrigin("USA");
+        jaeger1.setHeight(79.25f);
+        jaeger1.setWeight(1.98f);
+        jaeger1.setStrength(8);
+        jaeger1.setArmor(6);
+        jaeger1.setIsDestroyed(false);
+
+        Jaeger jaeger2 = new Jaeger();
+        jaeger2.setModelName("Coyote Tango");
+        jaeger2.setMark("Mark-1");
+        jaeger2.setOrigin("Japan");
+        jaeger2.setHeight(85.34f);
+        jaeger2.setWeight(2.312f);
+        jaeger2.setStrength(7);
+        jaeger2.setArmor(4);
+        jaeger2.setIsDestroyed(false);
+
+        jaeger1.drift();
+        jaeger2.drift();
+        System.out.println(jaeger1.getModelName() + " - " + jaeger1.move());
+        System.out.println(jaeger2.getModelName() + " - " + jaeger2.scanKaiju());
+        jaeger1.useCannon();
+        jaeger2.useCannon();
+        jaeger1.compareStrength(jaeger2.getStrength());
+        jaeger1.setStrength(7);
+        jaeger1.compareStrength(jaeger2.getStrength());
+        jaeger2.compareArmor(jaeger1.getArmor());
+        jaeger2.setArmor(7);
+        jaeger2.compareArmor(jaeger1.getArmor());
+        jaeger1.destroy();
+        jaeger2.destroy();
+        System.out.println("Робот " + jaeger1.getModelName() + " уничтожен? " + jaeger1.getIsDestroyed());
+        System.out.println("Робот " + jaeger2.getModelName() + " уничтожен? " + jaeger2.getIsDestroyed());
+    }
+}
