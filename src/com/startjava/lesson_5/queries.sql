0c58e5061427d871766d202c9299d330596fba45
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-10-09 20:42:57 +0700

    implement Lesson_5

---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,11 @@
+-- queries
+
+SELECT * FROM Jaegers;
+SELECT * FROM Jaegers WHERE status != 'Destroyed';
+SELECT * FROM Jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+SELECT * FROM Jaegers ORDER BY mark DESC;
+SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM Jaegers);
+SELECT AVG(weight) FROM Jaegers
+UPDATE Jaegers SET kaijukill = (kaijukill + 1) WHERE status != 'Destroyed';
+DELETE FROM Jaegers WHERE status = 'Destroyed';
