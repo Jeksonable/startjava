b568c3c392b192474a9ff886fa31e032085c4d15
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-10-10 21:31:28 +0700

    fix format Lesson_5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,13 +1,13 @@
 --create table
 
 CREATE TABLE Jaegers (
-	id 			SERIAL PRIMARY KEY,
-	modelName 	TEXT,
-	mark 		CHAR(6),
-	height 		DECIMAL,
-	weight 		DECIMAL,
-	status 		TEXT,
-	origin 		TEXT,
-	launch 		DATE,
-	kaijuKill 	INTEGER
+	id          SERIAL PRIMARY KEY,
+	modelName   TEXT,
+	mark        CHAR(6),
+	height      DECIMAL,
+	weight      DECIMAL,
+	status      TEXT,
+	origin      TEXT,
+	launch      DATE,
+	kaijuKill   INTEGER
 );
