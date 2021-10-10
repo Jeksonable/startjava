ffe7a6de536d3a737737f723b5008e59b131aa17
Evgeniy Erekaykin <jeksonable@gmail.com>
2021-10-10 21:38:24 +0700

    fix format v2 Lesson_5

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -2,13 +2,13 @@
 
 INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
 	VALUES
-		('Gipsy Danger', 	'Mark-3', '79.25', 	'1.980', 'Destroyed', 	'USA', 			'July 10, 2017', '9'),
-		('Cherno Alpha', 	'Mark-1', '85.34', 	'2.412', 'Destroyed', 	'Russia', 		'July 17, 2015', '6'),
-		('Striker Eureka', 	'Mark-5', '76.2', 	'1.850', 'Destroyed', 	'Australia',    'November 2, 2019', '11'),
-		('Coyote Tango', 	'Mark-1', '85.34', 	'2.312', 'Destroyed', 	'Japan', 		'December 30, 2015', '2'),
-		('Crimson Typhoon', 'Mark-4', '76.2',   '1.722', 'Single',      'China',        'August 22, 2018', '7'),
-		('Guardian Bravo', 	'Mark-6', '73.21', 	'1.975', 'Active',      'Japan',        'December 15, 2033', '1'),
-		('Saber Athena', 	'Mark-7', '76.91', 	'1.628', 'Destroyed', 	'USA', 			'December 20, 2033', '1'),
-		('Bracer Phoenix', 	'Mark-5', '70.91', 	'2.128', 'Destroyed', 	'USA', 			'November 1, 2025', '1'),
-		('Tacit Ronin', 	'Mark-1', '74.37', 	'7.450', 'Destroyed', 	'Japan', 		'December 6, 2015', '3'),
-		('Romeo Blue', 		'Mark-1', '77.72', 	'7.775', 'Destroyed', 	'USA', 			'December 15, 2015', '2');
+		('Gipsy Danger',    'Mark-3',   '79.25',    '1.980',    'Destroyed',    'USA',          'July 10, 2017',        '9'),
+		('Cherno Alpha',    'Mark-1',   '85.34',    '2.412',    'Destroyed',    'Russia',       'July 17, 2015',        '6'),
+		('Striker Eureka',  'Mark-5',   '76.20',    '1.850',    'Destroyed',    'Australia',    'November 2, 2019',     '11'),
+		('Coyote Tango',    'Mark-1',   '85.34',    '2.312',    'Destroyed',    'Japan',        'December 30, 2015',    '2'),
+		('Crimson Typhoon', 'Mark-4',   '76.20',    '1.722',    'Single',       'China',        'August 22, 2018',      '7'),
+		('Guardian Bravo',  'Mark-6',   '73.21',    '1.975',    'Active',       'Japan',        'December 15, 2033',    '1'),
+		('Saber Athena',    'Mark-7',   '76.91',    '1.628',    'Destroyed',    'USA',          'December 20, 2033',    '1'),
+		('Bracer Phoenix',  'Mark-5',   '70.91',    '2.128',    'Destroyed',    'USA',          'November 1, 2025',     '1'),
+		('Tacit Ronin',     'Mark-1',   '74.37',    '7.450',    'Destroyed',    'Japan',        'December 6, 2015',     '3'),
+		('Romeo Blue',      'Mark-1',   '77.72',    '7.775',    'Destroyed',    'USA',          'December 15, 2015',    '2');
